d4f6b2c6e39efa1120cd1d91f47c300558b51090
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-20 22:27:04 +0200

    feat(questions): add id

--- src/lib/questions.js
+++ src/lib/questions.js
@@ -1,5 +1,6 @@
 export const questions = [
   {
+    id: 1,
     imageURL: require("../assets/1.jpg"),
     question: "Zostawiłeś na stole ulubione ciasteczko- czy kot je zje?",
     answers: [
@@ -12,6 +13,7 @@ export const questions = [
       "Koty nie odczuwają słodkiego smaku, dlatego są to dla nich tylko puste kalorie.",
   },
   {
+    id: 2,
     imageURL: require("../assets/2.jpg"),
     question: "Czym pocą się koty?",
     answers: ["Główką", "Łapkami", "Całym ciałem"],
@@ -20,6 +22,7 @@ export const questions = [
       "Gruczoły potowe u kota znajdują się na poduszkach łapek. Koty pocą się nie tylko w celu wyrównania temperatury ciała, ale także gdy są niespokojne.",
   },
   {
+    id: 3,
     imageURL: require("../assets/3.jpg"),
     question: "Czy uszy kota są obrotowe?",
     answers: ["Tak, o 180 stopni", "Tak, o 90 stopni", "Nie"],
@@ -28,6 +31,7 @@ export const questions = [
       "Kot potrafi obrócić swoje ucho aż o 180 stopni. Uszy u kota są również sposobem na wychwycenie jego zmiany nastroju.",
   },
   {
+    id: 4,
     imageURL: require("../assets/4.jpg"),
     question: "Czy istnieją koty lewo-łapne?",
     answers: ["Tak, ale tylko Kotki.", "Nie.", "Tak, ale tylko kocury."],
@@ -36,6 +40,7 @@ export const questions = [
       "Według badań, kotki wykazują tendencję do bycia prawo- łapne, a kocury lewo-łapne.",
   },
   {
+    id: 5,
     imageURL: require("../assets/5.jpg"),
     question: "Ile różnych dźwięków są w stanie wydać koty?",
     answers: ["Ponad 50.", "Ponad 100.", "Ponad 150."],
@@ -43,6 +48,7 @@ export const questions = [
     quriosity: "Psy mogą natomiast wydawać tylko około 10.",
   },
   {
+    id: 6,
     imageURL: require("../assets/6.jpg"),
     question: "Kto lepiej słyszy?",
     answers: ["Pies", "Kot", "Zarówno pies i kot słyszą tak samo"],
@@ -51,6 +57,7 @@ export const questions = [
       "Kot jest w stanie także wychwytywać dźwięki o częstotliwości wyższej niż człowiek.",
   },
   {
+    id: 7,
     imageURL: require("../assets/7.jpg"),
     question: "Ile kosztował najdroższy kot na świecie?",
     answers: ["około 50 tys.", "około 100 tys.", "około 150 tys."],
@@ -59,6 +66,7 @@ export const questions = [
       "Był to kot Little Nicky i był pierwszym sklonowanym kotem na zamówienie.",
   },
   {
+    id: 8,
     imageURL: require("../assets/8.jpg"),
     question: "Czy koty mają unikalne linie papilarne?",
     answers: ["Tak", "Nie", "Tylko niektóre"],
@@ -67,6 +75,7 @@ export const questions = [
       "Okazuje się, że nie można znaleźć dwóch kotów o identycznym nosie. Co więcej koty swojego nosa nie widzą.",
   },
   {
+    id: 9,
     imageURL: require("../assets/9.jpg"),
     question: "Czy koty angażują się w politykę?",
     answers: [
@@ -79,6 +88,7 @@ export const questions = [
       "W mieście Talkeetna na Alasce, stanowisko burmistrza przez 20 lat obejmował kot. Został burmistrzem ponieważ, żaden inny kandydat nie przypadł do gustu mieszkańcom.",
   },
   {
+    id: 10,
     imageURL: require("../assets/10.jpg"),
     question: "Ile powiek ma kot?",
     answers: ["Dwie", "Trzy", "Cztery"],
@@ -87,6 +97,7 @@ export const questions = [
       "Trzecia powieka stanowi maleńki trójkącik różowawej lub białawej tkanki, zazwyczaj widoczny w rogu oku.",
   },
   {
+    id: 11,
     imageURL: require("../assets/11.jpg"),
     question: "Ile wąsów ma kot po obu stronach pyszczka?",
     answers: ["10", "12", "14"],
@@ -95,6 +106,7 @@ export const questions = [
       "Kot używa swoich wąsów, aby sprawdzić szerokość przejścia i czy się w nim zmieści.",
   },
   {
+    id: 12,
     imageURL: require("../assets/12.jpg"),
     question: "Które koty są przeważnie głuche?",
     answers: [
@@ -107,6 +119,7 @@ export const questions = [
       "Białe koty które mają tylko jedno oko niebieskie są głuche na to uch które znajduje się po stronie niebieskiego oka.",
   },
   {
+    id: 13,
     imageURL: require("../assets/13.jpg"),
     question: "Na jaką odległość widzą koty?",
     answers: [
@@ -118,6 +131,7 @@ export const questions = [
     quriosity: "Dodatkowo, kąt widzenia kota wynosi ok 185 stopni.",
   },
   {
+    id: 14,
     imageURL: require("../assets/14.jpg"),
     question: "Ile lat mogą żyć koty?",
     answers: ["Do 20 lat.", "Do 30 lat.", "Do 40 lat."],
@@ -125,6 +139,7 @@ export const questions = [
     quriosity: "Odpowiada to 98 ludzkim latom.",
   },
   {
+    id: 15,
     imageURL: require("../assets/15.jpg"),
     question: "Z jaką prędkością mruczą koty?",
     answers: [
@@ -137,6 +152,7 @@ export const questions = [
       "Średnio koty spędzają na mruczeniu ok 10 950 godzin. Koty mogą mruczeć zarówno podczas wdychania i wydychania powietrza.",
   },
   {
+    id: 16,
     imageURL: require("../assets/16.jpg"),
     question: "Czy Koty widzą w całkowitych ciemnościach?",
     answers: ["Tak", "Nie", "Tylko w gwieździste noce"],
@@ -145,6 +161,7 @@ export const questions = [
       "Pomimo tego, iż koty przystosowują się do widzenia w niepełnym oświetleniu, to potrzebują 1/6 światła wymaganego dla ludzkiego oka.",
   },
   {
+    id: 17,
     imageURL: require("../assets/17.jpg"),
     question: "Jaka jest normalna temperatura dla kociego ciała?",
     answers: [
@@ -157,6 +174,7 @@ export const questions = [
       "Z tego powody koty zmuszone są szukać dodatkowego źródła ciepła.",
   },
   {
+    id: 18,
     imageURL: require("../assets/18.jpg"),
     question: "Ile ważył największy kot domowy?",
     answers: ["18,5 kg", "21,3 kg", "24,7 kg"],
@@ -165,6 +183,7 @@ export const questions = [
       "Był to żyjący w  Australii kocur Himmy. Jego długość wynosiła prawie 1m.",
   },
   {
+    id: 19,
     imageURL: require("../assets/19.jpg"),
     question: "Ile kotków może liczyć jeden poród?",
     answers: [
